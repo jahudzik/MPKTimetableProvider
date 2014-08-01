479d246b1fe189e18cdc42828d1fbd3b60c80b59
Jan Hudzik <jhudzik@antennasoftware.com>
2014-08-01 16:48:33 +0200

    Added LineRouteParser with tests and sample cod

---
+++ MPKParser/src/pl/jahu/mpk/parser/exceptions/LineRouteParseException.java
@@ -0,0 +1,13 @@
+package pl.jahu.mpk.parser.exceptions;
+
+/**
+ * Created by hudzj on 8/1/2014.
+ */
+public class LineRouteParseException extends Exception {
+
+    public LineRouteParseException(String message) {
+        super(message);
+    }
+
+
+}
