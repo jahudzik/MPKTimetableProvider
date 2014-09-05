8c57b35469f7d960c065471598cd36ca53dfabbc
Jan Hudzik <jahudzik@op.pl>
2014-09-05 20:49:06 +0200

    Added transits generation (TransitBuilder) to integration tests

--- MPKParserTests/src/pl/jahu/mpk/tests/integration/ShowTimetableIntegrationTest.java
+++ MPKParserTests/src/pl/jahu/mpk/tests/integration/ShowTimetableIntegrationTest.java
@@ -2,7 +2,10 @@ package pl.jahu.mpk.tests.integration;
 
 import org.junit.AfterClass;
 import org.junit.Test;
+import pl.jahu.mpk.TransitBuilder;
 import pl.jahu.mpk.entities.Departure;
+import pl.jahu.mpk.entities.Timetable;
+import pl.jahu.mpk.entities.Transit;
 import pl.jahu.mpk.enums.DayTypes;
 import pl.jahu.mpk.parser.LineRouteParser;
 import pl.jahu.mpk.parser.LinesListParser;
@@ -10,7 +13,9 @@ import pl.jahu.mpk.parser.TimetableParser;
 import pl.jahu.mpk.parser.exceptions.LineRouteParseException;
 import pl.jahu.mpk.parser.exceptions.TimetableNotFoundException;
 import pl.jahu.mpk.parser.exceptions.TimetableParseException;
+import pl.jahu.mpk.validators.exceptions.TransitValidationException;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -27,12 +32,13 @@ public class ShowTimetableIntegrationTest {
     private static int actLine;
     private static String destination;
     private static String actTimetableUrl;
-    private static boolean printOutput;
+    private static boolean printGeneralOutput;
+    private static boolean printDetailedOutput;
 
     @Test
     public void testShowingTimetable() {
         try {
-            showTimetable(0, 1000, true);
+            showTimetable(0, 1000, true, false);
         } catch (TimetableNotFoundException e) {
             e.printStackTrace();
             fail();
@@ -42,18 +48,22 @@ public class ShowTimetableIntegrationTest {
         } catch (TimetableParseException e) {
             e.printStackTrace();
             fail();
+        } catch (TransitValidationException e) {
+            e.printStackTrace();
+            fail();
         }
     }
 
     @AfterClass
     public static void logAfter() {
-        if (printOutput) {
+        if (printGeneralOutput) {
             System.out.println("LAST EXECUTED: {line=" + actLine + ", destination='" + destination + "', url=" + actTimetableUrl + "}");
         }
     }
 
-    public static void showTimetable(int firstLine, int lastLine, boolean printOutput) throws TimetableNotFoundException, LineRouteParseException, TimetableParseException {
-        ShowTimetableIntegrationTest.printOutput = printOutput;
+    public static void showTimetable(int firstLine, int lastLine, boolean printGeneralOutput, boolean printDetailedOutput) throws TimetableNotFoundException, LineRouteParseException, TimetableParseException, TransitValidationException {
+        ShowTimetableIntegrationTest.printGeneralOutput = printGeneralOutput;
+        ShowTimetableIntegrationTest.printDetailedOutput = printDetailedOutput;
         LinesListParser linesListParser = new LinesListParser();
         List<Integer> lines = linesListParser.parse();
         assertNotNull(lines);
@@ -75,38 +85,53 @@ public class ShowTimetableIntegrationTest {
         for (int k = firstLineIndex; k <= lastLineIndex; k++) {
             int line = lines.get(k);
             actLine = line;
+
             // for each destination...
             for (int i = 1; i < 10; i++) {
                 try {
                     LineRouteParser routeParser = new LineRouteParser(line, i);
                     destination = routeParser.getDestination();
-                    // get route (all stations)
+
+                    // get all stations on the route
                     List<String[]> route = routeParser.parse();
                     assertNotNull(route);
                     assertTrue(route.size() > 0);
+                    List<Timetable> timetables = new ArrayList<Timetable>();
+
+                    // for each station on the route...
                     for (String[] station : route) {
                         assertNotNull(station);
                         assertEquals(2, station.length);
                         assertNotNull(station[0]);
                         assertNotNull(station[1]);
-                    }
 
-                    // parse timetable for the first station on the route
-                    TimetableParser timetableParser = new TimetableParser(line, route.get(0)[1]);
-                    actTimetableUrl = timetableParser.getUrl();
-                    Map<DayTypes, List<Departure>> timetables = timetableParser.parse().getDepartures();
-                    assertNotNull(timetables);
-                    assertTrue(timetables.size() > 0);
-
-                    StringBuilder sb = new StringBuilder();
-                    for (DayTypes dayType : timetables.keySet()) {
-                        List<Departure> departures = timetables.get(dayType);
-                        sb.append(dayType).append("=").append(departures.size()).append(";");
+                        // parse and save timetable
+                        TimetableParser timetableParser = new TimetableParser(line, station[1]);
+                        actTimetableUrl = timetableParser.getUrl();
+                        Timetable timetable = timetableParser.parse();
+                        Map<DayTypes, List<Departure>> departures = timetable.getDepartures();
                         assertNotNull(departures);
                         assertTrue(departures.size() > 0);
+                        timetables.add(timetable);
                     }
 
-                    if (printOutput) {
+                    // get list of all transits on the route (based on timetables)
+                    Map<DayTypes, List<Transit>> transitsMap = TransitBuilder.buildFromTimetables(timetables);
+                    assertNotNull(transitsMap);
+                    assertTrue(transitsMap.size() > 0);
+
+                    // prints all transits
+                    if (printDetailedOutput) {
+                        TransitBuilder.printTransitsMap(transitsMap);
+                    }
+
+                    // prints general summary of each destination of each line
+                    if (printGeneralOutput) {
+                        StringBuilder sb = new StringBuilder();
+                        for (DayTypes dayType : transitsMap.keySet()) {
+                            List<Transit> transits = transitsMap.get(dayType);
+                            sb.append(dayType).append("=").append(transits.size()).append(";");
+                        }
                         System.out.println("PASSED: {line=" + line + ", route='" + route.get(0)[0] + "'->'" + destination + "', stations=" + route.size() + ", departures={" + sb.toString() + "} }");
                     }
                 } catch (TimetableNotFoundException e) {
--- MPKParserTests/src/pl/jahu/mpk/tests/unit/integration/ShowTimetablePartialntegrationTest.java
+++ MPKParserTests/src/pl/jahu/mpk/tests/unit/integration/ShowTimetablePartialntegrationTest.java
@@ -5,6 +5,7 @@ import pl.jahu.mpk.parser.exceptions.LineRouteParseException;
 import pl.jahu.mpk.parser.exceptions.TimetableNotFoundException;
 import pl.jahu.mpk.parser.exceptions.TimetableParseException;
 import pl.jahu.mpk.tests.integration.ShowTimetableIntegrationTest;
+import pl.jahu.mpk.validators.exceptions.TransitValidationException;
 
 import static org.junit.Assert.fail;
 
@@ -18,7 +19,7 @@ public class ShowTimetablePartialntegrationTest {
     @Test
      public void testShowingDailyBusesTimetable() {
         try {
-            ShowTimetableIntegrationTest.showTimetable(180, 190, false);
+            ShowTimetableIntegrationTest.showTimetable(180, 190, false, false);
         } catch (TimetableNotFoundException e) {
             e.printStackTrace();
             fail();
@@ -26,6 +27,9 @@ public class ShowTimetablePartialntegrationTest {
             e.printStackTrace();
             fail();
         } catch (TimetableParseException e) {
+            e.printStackTrace();
+            fail();
+        } catch (TransitValidationException e) {
             e.printStackTrace();
             fail();
         }
@@ -34,7 +38,7 @@ public class ShowTimetablePartialntegrationTest {
     @Test
     public void testShowingNigthlyBusesTimetable() {
         try {
-            ShowTimetableIntegrationTest.showTimetable(600, 610, false);
+            ShowTimetableIntegrationTest.showTimetable(600, 610, false, false);
         } catch (TimetableNotFoundException e) {
             e.printStackTrace();
             fail();
@@ -42,6 +46,9 @@ public class ShowTimetablePartialntegrationTest {
             e.printStackTrace();
             fail();
         } catch (TimetableParseException e) {
+            e.printStackTrace();
+            fail();
+        } catch (TransitValidationException e) {
             e.printStackTrace();
             fail();
         }
