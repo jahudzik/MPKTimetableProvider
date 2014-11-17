425db15fdbfe744e46792b02e4418b3fc2b90b5f
Jan Hudzik <jahudzik@gmail.com>
2014-11-17 13:48:43 +0100

    Extended testing framework to parse timetables from specified location

--- MPKParserTests/unit/pl/jahu/mpk/DefaultTestModule.java
+++ MPKParserTests/unit/pl/jahu/mpk/DefaultTestModule.java
@@ -30,13 +30,25 @@ import javax.inject.Singleton;
 )
 public class DefaultTestModule {
 
-    public static final String FILES_LOCATION = "MPKParserTests/res";
+    private static final String TEST_FILES_LOCATION = "MPKParserTests/res/";
+    private static final String DEFAULT_TEST_FILES_DIRECTORY = "default";
+
+    private String filesLocation;
+
+
+    public DefaultTestModule() {
+        this(DEFAULT_TEST_FILES_DIRECTORY);
+    }
+
+    public DefaultTestModule(String filesDirectory) {
+        this.filesLocation = TEST_FILES_LOCATION + filesDirectory;
+    }
 
     @Provides
     @Singleton
     @SuppressWarnings("unused")
     TimetableProvider provideTimetableProvider() {
-        return new FileTimetableProvider(FILES_LOCATION);
+        return new FileTimetableProvider(filesLocation);
     }
 
 }
